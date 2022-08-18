34a0d7f49f89e9287485f2ac225d339736833b4e
Phonlatat Kathintip <https://gitlab.psu.ac.th/6210110224>
2022-08-18 19:27:24 +0700

    web

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,55 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Boostrap 5 Sass Starter`,
-    description: `A simple bootstrap 5 and Sass starter for Gatsby. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@r-ichard`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    pathPrefix: "/assign1a_task2",
+    siteMetadata: {
+        title: `ลงทะเบียนเรียน`,
+        description: `A simple bootstrap 5 and Sass starter for Gatsby. This barebones starter ships with the main Gatsby configuration files you might need.`,
+        author: `@r-ichard`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-sass`,
-      options: {
-        sassOptions: {
-          precision: 6,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-image`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
         },
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-bootstrap-5`,
-        short_name: `gb5-starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-gatsby-cloud`,
-  ],
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-sass`,
+            options: {
+                sassOptions: {
+                    precision: 6,
+                },
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-bootstrap-5`,
+                short_name: `gb5-starter`,
+                start_url: `/`,
+                background_color: `#663399`,
+                theme_color: `#663399`,
+                display: `standalone`,
+                icon: `src/images/iconweb.png`, // This path is relative to the root of the site.
+            },
+        },
+        {
+            resolve: "@directus/gatsby-source-directus",
+            options: {
+                url: `https://w8oi3vrm.directus.app/`, // Fill with your Directus instance address
+                auth: {
+                    token: "Mkj9EBcCpM9kD9P0HDS4gDf6c4_llgvl", // You can use a static token from an user
+
+                    // Or you can use the credentials of an user
+                    //email: "6210110342@psu.ac.th",
+                    //password: "admin",
+                },
+            },
+        },
+        `gatsby-plugin-gatsby-cloud`,
+    ],
 }
